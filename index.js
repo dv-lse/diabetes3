f1a7bfa70adb337f6c3c0816c1877aa47004e44e
Christopher W York <christopher.w.york@gmail.com>
2016-09-21 14:20:37 +0100

    Add dropdown with dataset names

--- index.js
+++ index.js
@@ -8,35 +8,55 @@ import Slider from './slider'
 
 import 'styles.css!'
 
-const METRICS = [ 'metric1', 'metric2', 'metric3' ]
+// pre-load datasets
 
-const weight_scale = d3.scaleQuantize()
-  .domain([0,100])
-  .range(d3.range(0,5))
+const API_URL = 'https://api.github.com/repos/dv-lse/diabetes2/contents/data'
 
-// state
+d3.json(API_URL, (err, files) => {
+  if(err) throw err
+  let datasets = files.map( (d) => d.path )
+  run(datasets)
+})
 
-let state = {
-}
+// user interface
 
-METRICS.forEach( (m) => state[m] = Slider(weight_scale) )
+function run(datasets) {
+  const METRICS = [ 'metric1', 'metric2', 'metric3' ]
 
-// view
+  const weight_scale = d3.scaleQuantize()
+    .domain([0,100])
+    .range(d3.range(0,5))
 
-function render(state) {
-  return h('div.weights',
-    METRICS.map( (metric) => Slider.render(state[metric], metric) )
-  )
-/*
-  [
-    Slider.render(state.metric1, fmt, 'Metric 1'),
-    svg('svg', {width: 800, height: 600},
-      svg('circle', {cx: 400, cy: 300, r: state.metric1.value * 3, fill: 'pink'})
+  // state
+
+  let state = {
+    dataset: datasets[0]
+  }
+
+  METRICS.forEach( (m) => state[m] = Slider(weight_scale) )
+
+  // view
+
+  function render(state) {
+    return h('div.controls', [
+      h('select.dataset', datasets.map( (ds) => {
+        return h('option', { value: ds, selected: ds === state.dataset }, ds)
+      })),
+      h('div.weights',
+        METRICS.map( (metric) => Slider.render(state[metric], metric) )
+      )]
     )
-  ])
-*/
+  /*
+    [
+      Slider.render(state.metric1, fmt, 'Metric 1'),
+      svg('svg', {width: 800, height: 600},
+        svg('circle', {cx: 400, cy: 300, r: state.metric1.value * 3, fill: 'pink'})
+      )
+    ])
+  */
+  }
+
+  // main loop
+
+  loop( () => render(state) )
 }
-
-// main loop
-
-loop( () => render(state) )
