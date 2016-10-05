49c94fed1a842cb3bce0013896233e8c1aeeff8f
Christopher W York <christopher.w.york@gmail.com>
2016-10-05 11:42:38 +0100

    More robust cache-busting for data file loads

--- index.js
+++ index.js
@@ -12,21 +12,17 @@ import 'styles.css!'
 
 // pre-load datasets
 
-const API_URL = 'https://api.github.com/repos/dv-lse/diabetes3/contents/data'
+const API_URL = 'https://api.github.com/repos/dv-lse/diabetes3/contents/data' + '?_=' + Date.now()
 const MARGINS = { top: 15, left: 50, bottom: 30, right: 15 }
 
-d3.json(API_URL, (err, files) => {
-  if(err) throw err
-
-  // TODO. cache busting code can be removed once github pages editing not longer needed
-  let cache_bust = Math.floor(Math.random() * 1000)
-  let paths = files.map( (f) => f.path )
+d3.json(API_URL, (err, dataFiles) => {
+  if(err) alert(err)
 
   let loader = queue()
-  paths.forEach( (path) => loader.defer(d3.csv, path + '?' + cache_bust ))
+  dataFiles.forEach( (d) => loader.defer(d3.csv, d.download_url + '?_=' + d.sha ))
   loader.awaitAll( (err, data) => {
-    if(err) throw err
-    let datasets = zipObject(data, (d,i) => paths[i])
+    if(err) alert(err)
+    let datasets = zipObject(data, (d,i) => dataFiles[i].name)
     run(datasets)
   })
 
